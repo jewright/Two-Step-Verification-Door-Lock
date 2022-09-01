e69fbad5c2d95b56efb36f110c4b4d93171b6995
Jordyn Wright <98404383+jewright@users.noreply.github.com>
2022-09-01 12:10:03 -0400

    Update reader.py

--- Scripts/rfid_scanner/reader.py
+++ Scripts/rfid_scanner/reader.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
+# imports : Raspberry Pi GIPO interface & RFID library 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
 
 try:
+        # Read Card ID
         id, text = reader.read()
+        
+        # Print ID data
         print(id)
         print(text)
+        
 finally:
         GPIO.cleanup()
