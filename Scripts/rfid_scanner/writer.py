a373f759abd5cdf26e9e22a653074fb93c46e846
Jordyn Wright <98404383+jewright@users.noreply.github.com>
2022-09-01 12:04:17 -0400

    Update writer.py

--- Scripts/rfid_scanner/writer.py
+++ Scripts/rfid_scanner/writer.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
+# imports : Raspberry Pi GIPO interface & RFID library 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 reader = SimpleMFRC522()
-
 try:
+        # Assign ID to Card
         text = input('New data:')
+        
+        # Place Card to assign the ID to data
         print("Now place your tag to write")
         reader.write(text)
         print("Written")
+        
 finally:
         GPIO.cleanup()
