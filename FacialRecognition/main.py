30a78f933e8af0e58d6ccd992c28826f020deae5
Jordyn Wright <98404383+jewright@users.noreply.github.com>
2022-09-09 16:53:11 -0400

    Delete FacialRecognition directory

--- FacialRecognition/main.py
+++
@@ -1,9 +0,0 @@
-# import all functions from each programs
-from dataset import dataset
-from recognition import rec
-from training import train
-
-# This will allow easy code switching
-# dataset()
-rec()
-# train()
