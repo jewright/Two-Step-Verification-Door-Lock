dbbd8b0644463a0af2c5667d48d9a1ffbb8962cb
Jordyn Wright <98404383+jewright@users.noreply.github.com>
2022-09-09 16:54:29 -0400

    Add files via upload

---
+++ Scripts/facial_recognition/dataset.py
@@ -0,0 +1,42 @@
+import cv2
+import os
+
+def dataset():
+    # Begin video feed
+    cam = cv2.VideoCapture(0)
+    # set video width
+    cam.set(3, 640)
+    # set video height
+    cam.set(4, 480)
+
+    # Detect Face
+    face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # For each person, enter one numeric face id
+    face_id = input('Enter User ID: ')
+    print("\n Beginning face capture...")
+    count = 0
+
+    while True:
+
+        ret, img = cam.read()
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            count += 1
+
+            # Save the captured image into the datasets folder
+            cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y + h, x:x + w])
+            cv2.imshow('image', img)
+
+        k = cv2.waitKey(100) & 0xff  # Press 'ESC' for exiting video
+        if k == 27:
+            break
+        elif count >= 30:  # Take 30 face sample and stop video
+            break
+
+    print("\n [INFO] Exiting Program and cleanup stuff")
+    cam.release()
+    cv2.destroyAllWindows()
---
+++ Scripts/facial_recognition/main.py
@@ -0,0 +1,9 @@
+# import all functions from each programs
+from dataset import dataset
+from recognition import rec
+from training import train
+
+# This will allow easy code switching
+#dataset()
+#train()
+rec()
